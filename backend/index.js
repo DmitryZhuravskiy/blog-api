88d4ce5e3d8f082c75888e7499f64279c9455530
Dmitry Zhuravskiy <dmitry.zhuravskiy@gmail.com>
2022-10-24 18:21:32 +0300

    first one

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,13 @@
 import express from "express";
 import mongoose from "mongoose";
-import { registerValidation } from "./validation/auth.js";
-import checkAuth from "./utils/checkAuth.js";
-import * as UserController from "./controllers/UserController.js";
+import {
+  registerValidation,
+  loginValidation,
+  postCreateValidation,
+} from "./validations.js";
+import { UserController, PostController } from './controllers/index.js';
+import multer from "multer";
+import { handleValidationErrors, checkAuth } from './utils/index.js';
 
 mongoose
   .connect(
@@ -14,15 +19,52 @@ mongoose
 const app = express();
 app.use(express.json());
 
+const storage = multer.diskStorage({
+  destination: (_, __, cb) => {
+    cb(null, "uploads");
+  },
+  filename: (_, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+
 app.get("/", (req, res) => {
   res.send("Dobro poshalovat!");
 });
 
-app.post("/auth/register", registerValidation, UserController.register);
-
-app.post("/auth/login", UserController.login);
-
+app.post(
+  "/auth/register",
+  registerValidation,
+  handleValidationErrors,
+  UserController.register
+);
+app.post(
+  "/auth/login",
+  loginValidation,
+  handleValidationErrors,
+  UserController.login
+);
 app.get("/auth/me", checkAuth, UserController.getMe);
+app.post("/upload", checkAuth, upload.single("image"), (req, res) => {
+  res.json({
+    url: `/uploads/${req.file.originalname}`,
+  });
+});
+app.use("/uploads", express.static("uploads"));
+
+app.get("/posts", PostController.getAll);
+//app.get('/posts/tags', PostController.getLastTags);
+app.get("/posts/:id", PostController.getOne);
+app.post("/posts", checkAuth, handleValidationErrors, PostController.create);
+app.delete("/posts/:id", checkAuth, PostController.remove);
+app.patch(
+  "/posts/:id",
+  checkAuth,
+  handleValidationErrors,
+  PostController.update
+);
 
 app.listen(4444, (err) => {
   if (err) {
