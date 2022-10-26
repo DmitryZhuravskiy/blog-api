26f898a23b2462e1dc008dd4b03899430f5894d1
Dmitry Zhuravskiy <dmitry.zhuravskiy@gmail.com>
2022-10-26 13:38:16 +0300

    get one

---
+++ frontend/src/pages/Login/index.jsx
@@ -0,0 +1,73 @@
+import React from "react";
+import Typography from "@mui/material/Typography";
+import TextField from "@mui/material/TextField";
+import Paper from "@mui/material/Paper";
+import Button from "@mui/material/Button";
+import { useForm } from "react-hook-form";
+import styles from "./Login.module.scss";
+import { fetchAuth, selectIsAuth } from "../../redux/slices/auth";
+import { useDispatch, useSelector } from "react-redux";
+import { Navigate } from "react-router-dom";
+import { alertTitleClasses } from "@mui/material";
+
+export const Login = () => {
+  const isAuth = useSelector(selectIsAuth);
+  const dispatch = useDispatch();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm({
+    defaultValues: {
+      email: "test@test.ru",
+      password: "123",
+    },
+    mode: "onChange",
+  });
+
+  const onSubmit = async (values) => {
+    const data = await dispatch(fetchAuth(values));
+
+    if (!data.payload) {
+      return alert(`не удалось авторизоваться`);
+    }
+
+    if (`token` in data.payload) {
+      window.localStorage.setItem(`token`, data.payload.token);
+    } else {
+      alert(`НЕ удалось загрузить данные`);
+    }
+  };
+
+  if (isAuth) {
+    return <Navigate to="/" />;
+  }
+
+  return (
+    <Paper classes={{ root: styles.root }}>
+      <Typography classes={{ root: styles.title }} variant="h5">
+        Вход в аккаунт
+      </Typography>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <TextField
+          className={styles.field}
+          label="E-Mail"
+          error
+          helperText={errors.email?.message}
+          {...register("email", { required: "Укажите почту" })}
+          fullWidth
+        />
+        <TextField
+          className={styles.field}
+          label="Пароль"
+          {...register("password", { required: "Укажите пароль" })}
+          helperText={errors.password?.message}
+          fullWidth
+        />
+        <Button type="submit" size="large" variant="contained" fullWidth>
+          Войти
+        </Button>
+      </form>
+    </Paper>
+  );
+};
