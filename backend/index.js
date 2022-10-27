142e6494679c87c7cc0c1d8d22a1f0863fb38ada
Dmitry Zhuravskiy <dmitry.zhuravskiy@gmail.com>
2022-10-27 20:00:37 +0300

    add post

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,14 @@
 import express from "express";
 import mongoose from "mongoose";
-import { registerValidation } from "./validation/auth.js";
-import checkAuth from "./utils/checkAuth.js";
-import * as UserController from "./controllers/UserController.js";
+import {
+  registerValidation,
+  loginValidation,
+  postCreateValidation,
+} from "./validations.js";
+import { UserController, PostController } from "./controllers/index.js";
+import multer from "multer";
+import { handleValidationErrors, checkAuth } from "./utils/index.js";
+import cors from "cors";
 
 mongoose
   .connect(
@@ -12,17 +18,56 @@ mongoose
   .catch((err) => console.log("DB is error", err));
 
 const app = express();
+app.use(cors());
 app.use(express.json());
+
+const storage = multer.diskStorage({
+  destination: (_, __, cb) => {
+    cb(null, "uploads");
+  },
+  filename: (_, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage });
 
 app.get("/", (req, res) => {
   res.send("Dobro poshalovat!");
 });
 
-app.post("/auth/register", registerValidation, UserController.register);
-
-app.post("/auth/login", UserController.login);
-
+app.post(
+  "/auth/register",
+  registerValidation,
+  handleValidationErrors,
+  UserController.register
+);
+app.post(
+  "/auth/login",
+  loginValidation,
+  handleValidationErrors,
+  UserController.login
+);
 app.get("/auth/me", checkAuth, UserController.getMe);
+app.post("/upload", checkAuth, upload.single("image"), (req, res) => {
+  res.json({
+    url: `/uploads/${req.file.originalname}`,
+  });
+});
+app.use('/uploads', express.static('uploads'));
+
+app.get('/tags', PostController.getLastTags);
+app.get("/posts", PostController.getAll);
+app.get('/posts/tags', PostController.getLastTags);
+app.get("/posts/:id", PostController.getOne);
+app.post("/posts", checkAuth, handleValidationErrors, PostController.create);
+app.delete("/posts/:id", checkAuth, PostController.remove);
+app.patch(
+  "/posts/:id",
+  checkAuth,
+  handleValidationErrors,
+  PostController.update
+);
 
 app.listen(4444, (err) => {
   if (err) {
---
+++ frontend/src/pages/AddPost/index.jsx
@@ -0,0 +1,149 @@
+import React from "react";
+import { useNavigate, Navigate, useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+import TextField from "@mui/material/TextField";
+import Paper from "@mui/material/Paper";
+import Button from "@mui/material/Button";
+import SimpleMDE from "react-simplemde-editor";
+
+import "easymde/dist/easymde.min.css";
+import { selectIsAuth } from "../../redux/slices/auth";
+import axios from "../../axios";
+import styles from "./AddPost.module.scss";
+
+export const AddPost = () => {
+  const navigate = useNavigate();
+  const isAuth = useSelector(selectIsAuth);
+  const [text, setText] = React.useState("");
+  const [title, setTitle] = React.useState("");
+  const [tags, setTags] = React.useState("");
+  const inputFileRef = React.useRef(null);
+  const [imageUrl, setImageUrl] = React.useState();
+  const [isLoading, setLoading] = React.useState(false);
+
+  const handleChangeFile = async (event) => {
+    try {
+      const formData = new FormData();
+      const file = event.target.files[0];
+      formData.append("image", file);
+      const { data } = await axios.post("/upload", formData);
+      setImageUrl(file.name);
+      console.log(file.name);
+    } catch (err) {
+      console.warn(err);
+      alert("Ошибка при загрузке файла!");
+    }
+  };
+
+  const onClickRemoveImage = () => {
+    setImageUrl('');
+  };
+
+  const onChange = React.useCallback((value) => {
+    setText(value);
+  }, []);
+
+  const options = React.useMemo(
+    () => ({
+      spellChecker: false,
+      maxHeight: "400px",
+      autofocus: true,
+      placeholder: "Введите текст...",
+      status: false,
+      autosave: {
+        enabled: true,
+        delay: 1000,
+      },
+    }),
+    []
+  );
+
+  const onSubmit = async () => {
+    try {
+      setLoading(true);
+      const fields = {
+        title,
+        imageUrl,
+        tags: tags.split(`,`),
+        text,
+      };
+      const { data } = await axios.post("/posts", fields);
+      const id = data._id;
+      navigate(`/posts/${id}`)
+    } catch (err) {
+      console.warn(err);
+      alert('Ошибка при создании статьи!');
+    }
+  };
+
+  if (!window.localStorage.getItem("token") && !isAuth) {
+    return <Navigate to="/" />;
+  }
+
+  return (
+    <Paper style={{ padding: 30 }}>
+      <Button
+        onClick={() => inputFileRef.current.click()}
+        variant="outlined"
+        size="large"
+      >
+        Загрузить превью
+      </Button>
+      <input
+        ref={inputFileRef}
+        type="file"
+        onChange={handleChangeFile}
+        hidden
+      />
+      {imageUrl && (
+        <>
+          <Button
+            variant="contained"
+            color="error"
+            onClick={onClickRemoveImage}
+          >
+            Удалить
+          </Button>
+          <img
+            className={styles.image}
+            src={`http://localhost:4444/uploads/${imageUrl}`}
+            alt="Uploaded"
+          />
+        </>
+      )}
+
+      <br />
+      <br />
+      <TextField
+        classes={{ root: styles.title }}
+        variant="standard"
+        placeholder="Заголовок статьи..."
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        fullWidth
+      />
+      <TextField
+        classes={{ root: styles.tags }}
+        variant="standard"
+        placeholder="Тэги"
+        value={tags}
+        onChange={(e) => setTags(e.target.value)}
+        fullWidth
+      />
+      <SimpleMDE
+        className={styles.editor}
+        value={text}
+        onChange={onChange}
+        options={options}
+      />
+      <div className={styles.buttons}>
+        <Button size="large" variant="contained" onClick={onSubmit}>
+          Опубликовать
+        </Button>
+        <a href="/">
+          <Button size="large">Отмена</Button>
+        </a>
+      </div>
+    </Paper>
+  );
+};
